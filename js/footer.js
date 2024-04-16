ee5c579e91fefbe82ec05142ffe491fb6124c5ef
Choi <wldnd12223@gmail.com>
2024-04-16 18:07:15 +0900

    first commit

--- js/footer.js
+++ js/footer.js
@@ -5,8 +5,8 @@ footer.innerHTML = `
 
 <div class="bottom">
         <footer>
-            한국직업교육원 | 마케팅 업체: 플로스 | 사업자번호: 471-09-01112 | 주소: 서울 영등포구 선유로 53길 22 502호 | 제휴 문의 : flosmarketing@naver.com<br>
-            FLOS (C) All rights reserved
+            한국직업교육원<br>
+            한국직업교육원 (C) All rights reserved
         </footer>
     </div>
 
