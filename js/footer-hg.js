d0f440df3da54b199dd7d31850bfe52ddc3db19b
minwookang5687 <141393681+minwookang5687@users.noreply.github.com>
2024-07-04 20:27:15 +0900

    Add files via upload

--- js/footer-hg.js
+++ js/footer-hg.js
@@ -5,7 +5,7 @@ footer.innerHTML = `
 
 <div class="bottom">
         <footer>
-        FLOS © All Rights Reserved<br>
+        리코코퍼레이션 © All Rights Reserved<br>
         상호 : 애드라인 | 대표이사 : 이현서 | 사업자등록번호 : 619-62-00442<br>
         주소 : 대전광역시 서구 관저남로12번길 12.104동1202호(관저동,중흥s-클래스 프라디움)<br>
         건강기능식품판매업 : 제2020-0239478호 | 건강기능식품광고심의필 : 180110001<br>
