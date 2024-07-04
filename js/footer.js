49245a2cbf5f55e99c2562d7c3dcc009423bc8fd
minwookang5687 <141393681+minwookang5687@users.noreply.github.com>
2024-07-04 19:23:34 +0900

    Add files via upload

--- js/footer.js
+++ js/footer.js
@@ -5,9 +5,7 @@ footer.innerHTML = `
 
 <div class="bottom">
         <footer>
-            한국직업교육원<br>
-            한국직업교육원 (C) All rights reserved <br><br>
-            리코코퍼레이션<br>
+            리코코퍼레이션 (C) All rights reserved <br>
             대표자: 강민우 l 개인정보보호책임자: 김선재<br>
             사업자등록번호: 366-03-03344<br>
             사업자소재지: 서울특별시 마포구 서교동 473-37 203
