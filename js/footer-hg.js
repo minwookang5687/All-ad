2c2086d7eaf833273d4d21783e90975a5363e743
minwookang5687 <141393681+minwookang5687@users.noreply.github.com>
2024-08-02 05:45:35 +0900

    Add files via upload

--- js/footer-hg.js
+++ js/footer-hg.js
@@ -5,11 +5,11 @@ footer.innerHTML = `
 
 <div class="bottom">
         <footer>
+            카슐랭
             리코코퍼레이션 (C) All rights reserved <br>
             대표자: 강민우 l 개인정보보호책임자: 김선재<br>
             사업자등록번호: 366-03-03344<br>
             사업자소재지: 서울특별시 마포구 서교동 473-37 203<br>
-            Contact: mr.emwoo@gmail.com
         </footer>
     </div>
 
