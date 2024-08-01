87db270a98b2558ef2052aea3fd130a2edac61ce
minwookang5687 <141393681+minwookang5687@users.noreply.github.com>
2024-08-02 06:14:01 +0900

    Add files via upload

--- js/footer-hg.js
+++ js/footer-hg.js
@@ -5,7 +5,7 @@ footer.innerHTML = `
 
 <div class="bottom">
         <footer>
-            카슐랭
+            장기렌트·리스 전문 카슐랭<br>
             리코코퍼레이션 (C) All rights reserved <br>
             대표자: 강민우 l 개인정보보호책임자: 김선재<br>
             사업자등록번호: 366-03-03344<br>
