dbdc8c8ffb312976e7eb7d3064ea8de659f21128
minwookang5687 <141393681+minwookang5687@users.noreply.github.com>
2024-10-01 19:05:48 +0900

    Add files via upload

---
+++ js/footer-rent.js
@@ -0,0 +1,18 @@
+
+
+const footer = document.getElementById('footer');
+footer.innerHTML = `
+
+<div class="bottom">
+        <footer>
+            장기렌트·리스 전문 카슐랭<br>
+            리코코퍼레이션 (C) All rights reserved <br>
+            개인정보보호책임자: 김선재<br>
+            사업자등록번호: 366-03-03344<br>
+            사업자소재지: 서울특별시 마포구 서교동 473-37 203<br>
+        </footer>
+    </div>
+
+`;
+
+document.body.appendChild(footer.content);
