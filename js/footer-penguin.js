100aee77c3802b37ec67e42c2d1f836b6c3a6509
minwookang5687 <141393681+minwookang5687@users.noreply.github.com>
2025-07-28 02:41:06 +0900

    Add files via upload

--- js/footer-penguin.js
+++ js/footer-penguin.js
@@ -5,7 +5,7 @@ footer.innerHTML = `
 
 <div class="bottom">
         <footer>
-            펭귄통신 | 유한회사 위드컴퍼니<br>
+            한국통신비공정관리지원<br>
             <a href="https://freelicense.shop/Internet_consent">[유선통신 사전승낙서]</a> | <a href="https://freelicense.shop/phone_consent">[이동통신서비스 사전승낙서]</a><br>
             리코 코퍼레이션 (C) All rights reserved <br>
             개인정보보호책임자: 김선재<br>
