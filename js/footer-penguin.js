5b0f0678fc6c9cc1720a5a0d3534b6d0d561e9e7
minwookang5687 <141393681+minwookang5687@users.noreply.github.com>
2025-08-13 15:59:31 +0900

    Add files via upload

--- js/footer-penguin.js
+++ js/footer-penguin.js
@@ -5,10 +5,11 @@ footer.innerHTML = `
 
 <div class="bottom">
         <footer>
-            펭귄통신<br>
+            COPYRIGHT(c) 펭귄통신. ALL RIGHTS RESERVED.<br>
+            유한회사 위드컴퍼니<br>
+            사업자등록번호 : 398-81-01808 l 주소 : 전북 전주시 완산구 쑥고개로 315, 6층(효자동2가)<br>
+            대표전화 : 1577-6361 l 이메일 : oztop2@naver.com l 개인정보 책임관리자 : 방종헌(oztop2@naver.com)<br>
             <a href="https://freelicense.shop/Internet_consent">[유선통신 사전승낙서]</a> | <a href="https://freelicense.shop/phone_consent">[이동통신서비스 사전승낙서]</a><br>
-            리코 코퍼레이션 (C) All rights reserved <br>
-            개인정보보호책임자: 김선재<br>
         </footer>
     </div>
 
