222195975c445c6325635ab7b6cd7e8caf08d25c
minwookang5687 <141393681+minwookang5687@users.noreply.github.com>
2025-08-25 00:06:03 +0900

    Add files via upload

--- js/footer-penguin-new.js
+++ js/footer-penguin-new.js
@@ -5,8 +5,8 @@ footer.innerHTML = `
 
 <div class="bottom">
         <footer>
-            COPYRIGHT(c) 통신공정관리지원. ALL RIGHTS RESERVED.<br>
-            리코 코퍼레이션<br>
+            통신공정관리지원.<br>
+            리코 코퍼레이션(c) ALL RIGHTS RESERVED.<br>
             사업자등록번호 : 366-03-03344<br>
             개인정보관리책임자: 김선재<br>
             <a href="https://freelicense.shop/Internet_consent">[유선통신 사전승낙서]</a> | <a href="https://freelicense.shop/phone_consent">[이동통신서비스 사전승낙서]</a><br>
